e5f6844c3b9ad560f30e404239861fd469438c62
Eddin Amaya <badreddin.amaya@skola.botkyrka.se>
2021-02-19 17:22:37 +0100

    first

---
+++ main.lua
@@ -0,0 +1,20 @@
+
+love.load = function ()
+    print('The Game hass started')
+end
+
+love.update = function ()
+    print('uptaderat')
+
+
+end
+
+love.draw = function ()
+
+    love.graphics.circle(mode,x,y,radius)
+        local x = 50
+        local y = 100
+        local r = 50 
+    
+    print('draw')
+end
