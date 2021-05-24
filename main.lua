879bd44d754e8a8394cbaf203228559d1aef7e8f
Eddin Amaya <badreddin.amaya@skola.botkyrka.se>
2021-05-24 14:05:09 +0200

    Timer 10 sec intervals

--- main.lua
+++ main.lua
@@ -1,6 +1,4 @@
-
 local runGame = false
-
 local player = {
     x = 200,
     y = 550,
@@ -13,6 +11,18 @@ local obstacles = {{
     r = 30
 }, {
     x = 100,
+    y = 100,
+    r = 30
+}, {
+    x = 300,
+    y = 100,
+    r = 30
+}, {
+    x = 170,
+    y = 100,
+    r = 30
+}, {
+    x = 150,
     y = 100,
     r = 30
 }}
@@ -32,6 +42,8 @@ local menu = {{
 }}
 
 local menuIndex = 2
+
+local lastTime = love.timer.getTime()
 
 love.load = function()
     print('The Game hass started')
@@ -68,6 +80,12 @@ love.update = function()
 
     if runGame == true then
 
+        local currentTime = love.timer.getTime()
+        if currentTime - lastTime > 10 then
+            print("10")
+            lastTime = currentTime
+        end
+
         if love.keyboard.isDown("d") then
             player.x = player.x + 5
         end
@@ -83,7 +101,6 @@ love.update = function()
         if love.keyboard.isDown("s") then
             player.y = player.y + 5
         end
-
         -- Bollarnas förflyttning:
         for i = 1, #obstacles do
             obstacles[i].y = obstacles[i].y + 3
@@ -100,7 +117,6 @@ love.update = function()
         end
 
         for i = 1, #obstacles do
-            -- om Distansen mellan medelpunkterna på circlarna är mindre än radien på båda cirklar då krashar dem med varandra
             if math.sqrt((obstacles[i].x - player.x) ^ 2 + (obstacles[i].y - player.y) ^ 2) <
                 (player.r + obstacles[i].r) then
                 runGame = false
@@ -128,7 +144,6 @@ love.draw = function()
         for i = 1, #menu do
             if i == menuIndex then
                 love.graphics.setColor(0, 0, 1)
-
             else
                 love.graphics.setColor(1, 1, 1)
             end
